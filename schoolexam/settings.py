d3380f6b3fee3f6dd6fb35e909fbf7407fbcf20d
Bittu <pk.idea2code@gmail.com>
2022-03-04 12:22:59 +0530

    done

--- schoolexam/settings.py
+++ schoolexam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,8 @@ SECRET_KEY = '@(=&tdz6*1^h(v4**25t_@%+1xp30m$*3-h4f)56g+%gr1$z&+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://django-crud-demo.herokuapp.com/",
+                 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
